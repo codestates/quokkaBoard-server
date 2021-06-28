3761830e82ca8383fb50eb5fc98a8d2f65dafeaf
skyupguf <skyupguf@gmail.com>
2021-06-29 04:16:14 +0900

    Fix cookieset to deploy test

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { Request, Response } from 'express';
-// import { generateAccessToken, generateRefreshToken } from 'util/token';
+import { mintAccessToken, mintRefreshToken } from 'token/jwt';
+import 'dotenv/config';
+
 
 const app = express();
-const port = 4000;
+const port = process.env.SERVER_PORT || 4000;
 
 app.use(express.json());
 app.use(logger('dev'));
@@ -16,31 +18,37 @@ app.use(cors({
     origin: true,
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT"]
-}))
+}));
 
-//////////////////////////////////////// https server test
-// let console: Console
+/////////////////   server test  /////////////////////////
 
-// app.post('/user/login', (req: Request, res: Response) => {
-//     console.log('Hello TypeScript!')
+app.post('/user/login', (req: Request, res: Response) => {
+    console.log('Hello TypeScript!')
     
-//     const {id, email} = req.body
-//     if(id !== 'number' || email !== 'string') res.status(202).send('not fit')
+    const {id, email} = req.body
+    if(id !== 'string' || email !== 'string') res.status(202).send('not fit')
     
-//     const accessToken = generateAccessToken({id, email});
-//     const refreshToken = generateRefreshToken({id, email});
+    const accessToken = mintAccessToken({id, email});
+    const refreshToken = mintRefreshToken({id, email});
     
-//     res.cookie('Refresh Token:', refreshToken, {
-//         httpOnly: true,
-//         sameSite: 'none',
-//         secure: true
-//     });
-//     res.status(200).send({data: {accessToken: accessToken}, message: 'ok'});
-// });
-////////////////////////////////////////
+    res.cookie('Refresh Token:', refreshToken, {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true
+    });
+    res.status(200).send({data: {accessToken: accessToken}, message: 'ok'});
+});
+
+// const certKey = fs.readFileSync(__dirname + "/cert.pem", "utf-8");
+// const privKey = fs.readFileSync(__dirname + "/key.pem", "utf-8");
+// const asymmetricKey = { key: privKey, cert: certKey };
+
+// let server = https.createServer(asymmetricKey, app)
+// .listen(port, () => console.log(`server listening on ${port}`))
+
+/////////////////////////////////////////////////////////
+
 
 app.listen(port, () => {
     console.log(`server listening ${port}`);
 });
-
-module.exports = app;
