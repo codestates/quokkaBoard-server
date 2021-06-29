fc4de0aede40c6c5ad7eab956484b4ecd981aa82
skyupguf <skyupguf@gmail.com>
2021-06-29 18:45:45 +0900

    Refactor directory

--- src/token/jwt.ts
+++ src/token/jwt.ts
@@ -1,8 +1,7 @@
 import { sign, verify, Secret } from "jsonwebtoken";
 import "dotenv/config";
 
-export const mintAccessToken = (data: {id: string, email: string}) => {
-    
+export const mintAccessToken = (data: {id: string, email: string}) => {    
     return sign(
         data, 
         process.env.ACCESS_SALT as Secret, 
@@ -11,7 +10,6 @@ export const mintAccessToken = (data: {id: string, email: string}) => {
 }
   
 export const mintRefreshToken = (data: {id: string, email: string}) => {
-    
     return sign(
         data, 
         process.env.REFRESH_SALT as Secret, 
@@ -20,7 +18,6 @@ export const mintRefreshToken = (data: {id: string, email: string}) => {
 }
   
 export const checkAuthority = (auth: string | null) => {
-    
     if (auth === null) return null;
     const token = auth.split(' ')[1];
 
@@ -33,7 +30,6 @@ export const checkAuthority = (auth: string | null) => {
 };
 
 export const certifyToken = (refreshToken: string | null) => {
-
     if (refreshToken === null) return null;
 
     try {
