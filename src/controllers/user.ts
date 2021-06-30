889c15a62be7c4272a6b2e8c02bc0b47ac00cfe5
skyupguf <skyupguf@gmail.com>
2021-06-30 18:55:57 +0900

    test db

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '@entity/User';
 import jwtToken from '@token/jwt';
+
+
 
 const user = {
 
