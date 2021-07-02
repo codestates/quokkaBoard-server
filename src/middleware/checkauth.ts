75b834a020044b295223db1a23c5e1dac3c64eb4
skyupguf <skyupguf@gmail.com>
2021-07-03 00:30:31 +0900

    fix verify refToken

--- src/middleware/checkauth.ts
+++ src/middleware/checkauth.ts
@@ -1,7 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UserRepo } from '@repo/userDm';
-import { UserInfo } from "@types";
 import jwtToken from '@token/jwt';
 import 'dotenv/config';
 
@@ -13,17 +12,20 @@ export const checkAuthority = async (req: Request, res: Response, next: NextFunc
         success: false, message: '로그인 상태가 아닙니다'
     });
 
-    const { email } = req.body as UserInfo
-    const accPayload = jwtToken.checkAccToken(auth);
+    const accPayload = jwtToken.checkAccToken(auth);// 
     const customUserRepo = getCustomRepository(UserRepo);
-    const findUser = await customUserRepo.findEmail(email);
+    const findUser = await customUserRepo.findId(accPayload?.id!)
     const refPayload = jwtToken.checkRefToken(findUser?.refresh_token!);
 
     if(accPayload) next();
     else {
         if(refPayload) {
-            const accToken = jwtToken.mintAccessToken({id: findUser?.id!, email: email});
-            res.cookie('accessToken', accToken, { httpOnly: true, sameSite: 'none', secure: true });
+            const accToken = jwtToken.mintAccessToken({
+                id: findUser?.id, email: findUser?.email!
+            });
+            res.cookie('accessToken', accToken, { 
+                httpOnly: true, sameSite: 'none', secure: true 
+            });
             next();
         } else {
             res.status(202).send({ success: false, message: '다시 로그인해 주십시오' });
