bc85ea22339af835c5a3f66ad843f4a8f6d363e5
skyupguf <skyupguf@gmail.com>
2021-07-03 00:32:16 +0900

    fix jwt logic

--- src/token/jwt.ts
+++ src/token/jwt.ts
@@ -1,48 +1,46 @@
-import { sign, verify, Secret } from "jsonwebtoken";
-import { UserInfo } from "@types"
+import { sign, verify, Secret, JwtPayload } from "jsonwebtoken";
+import { UserInfo } from "@types";
 import "dotenv/config";
 
 
 const jwtToken = {
     
-    mintAccessToken: (payload: UserInfo) => {    
-     
-        return sign(payload, 
-            process.env.ACCESS_SALT as Secret, 
+    mintAccessToken: (payload: UserInfo) => {
+        return sign(
+            payload, process.env.ACCESS_SALT as Secret, 
             { expiresIn: '1h' }
         );
     },
       
     mintRefreshToken: (payload: UserInfo) => {
-        
-        return sign(payload, 
-            process.env.REFRESH_SALT as Secret, 
+        return sign(
+            payload, process.env.REFRESH_SALT as Secret, 
             { expiresIn: '7d' }
         );
     },
-      
-    checkAuthority: (auth: string | null) => {
-        
-        if (auth === null) return null;
+    
+    checkAccToken: (auth: string) => {
+
         const token = auth.split(' ')[1];
         try {
-            return verify(token, process.env.ACCESS_SALT as Secret)
-        } catch (err) {
-            console.error(err.message);
-            return 'Invalid user';
+            return <JwtPayload> verify(token, 
+                process.env.ACCESS_SALT as Secret
+            );
+        } catch (e) {
+            return null;
         }
     },
-    
-    certifyToken: (refreshToken: string | null) => {
+
+    checkRefToken: (refreshToken: string) => {
         
-        if (refreshToken === null) return null;
         try {
-            return verify(refreshToken, process.env.REFRESH_SALT as Secret)
-        } catch (err) {
-            console.error(err.message);
-            return 'Invalid token';
+            return <JwtPayload> verify(refreshToken, 
+                process.env.REFRESH_SALT as Secret
+            );
+        } catch (e) {
+            return null;
         }
-    }
+    },  
 }
 
 export default jwtToken;
