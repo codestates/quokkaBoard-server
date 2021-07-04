dda4c52ff081ef0f1e3e0ed06fa9e637827b8b0c
skyupguf <skyupguf@gmail.com>
2021-07-04 09:13:26 +0900

    Refactor checkauth

--- src/middleware/checkauth.ts
+++ src/middleware/checkauth.ts
@@ -8,27 +8,32 @@ import 'dotenv/config';
 export const checkAuthority = async (req: Request, res: Response, next: NextFunction) => {
 
     const auth = req.headers.authorization;
-    if (auth === undefined) return res.status(400).send({
+    if(!auth) return res.status(400).send({
         success: false, message: '로그인 상태가 아닙니다'
     });
-
-    const accPayload = jwtToken.checkAccToken(auth);// 
-    const customUserRepo = getCustomRepository(UserRepo);
-    const findUser = await customUserRepo.findId(accPayload?.id!)
-    const refPayload = jwtToken.checkRefToken(findUser?.refresh_token!);
-
+    const token = auth.split(" ")[1];
+    const accPayload = jwtToken.checkAccToken(token);
+    
     if(accPayload) next();
     else {
+        const expPayload = jwtToken.checkExpToken(token);
+        const customUserRepo = getCustomRepository(UserRepo);
+        const findUser = await customUserRepo.findId(expPayload.id)
+        if(!findUser) return res.status(401).send({
+            success: false, message: '인증된 사용자가 아닙니다'
+        });
+
+        const refPayload = jwtToken.checkRefToken(findUser.refresh_token!);
         if(refPayload) {
-            const accToken = jwtToken.mintAccessToken({
-                id: findUser?.id, email: findUser?.email!
-            });
+            const accToken = jwtToken.mintAccessToken(findUser.id);
             res.cookie('accessToken', accToken, { 
                 httpOnly: true, sameSite: 'none', secure: true 
             });
             next();
         } else {
-            res.status(202).send({ success: false, message: '다시 로그인해 주십시오' });
+            res.status(202).send({ 
+                success: false, message: '다시 로그인해 주십시오' 
+            });
         }
     }
 }
