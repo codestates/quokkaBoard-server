e3b61cbbe18dbb5c65fe4017f2f4eb7e510409a5
skyupguf <skyupguf@gmail.com>
2021-07-04 09:17:40 +0900

    Add modify controller & fix user controller

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,20 +2,20 @@ import { Request, Response } from 'express';
 import { getCustomRepository, getRepository } from 'typeorm';
 import { UserRepo } from '@repo/userDm';
 import { User } from '@entity/User';
-import { UserInfo, UserReq } from '@types';
+import { typeReq, strProps } from '@types';
 import jwtToken from '@token/jwt';
 
 
 const user = {
 
-    register: async (req: UserReq<UserInfo>, res: Response) => {
+    register: async (req: typeReq<strProps>, res: Response) => {
         
         const userRepo = getRepository(User)
         const { email, nickname, password } = req.body;
         const newUser = new User();
         newUser.email = email;
-        newUser.nickname = nickname!;
-        newUser.password = password!;
+        newUser.nickname = nickname;
+        newUser.password = password;
         newUser.hashPass();
 
         try {
@@ -27,7 +27,7 @@ const user = {
 
     },
 
-    existEmail: async (req: UserReq<UserInfo>, res: Response) => {
+    existEmail: async (req: typeReq<strProps>, res: Response) => {
 
         const customUserRepo = getCustomRepository(UserRepo)
         const findUser = await customUserRepo.findEmail(req.body.email);
@@ -36,16 +36,16 @@ const user = {
 
     },
 
-    existNickName: async (req: UserReq<UserInfo>, res: Response) => {
+    existNickName: async (req: typeReq<strProps>, res: Response) => {
         
         const customUserRepo = getCustomRepository(UserRepo)
-        const findUser = await customUserRepo.findNickName(req.body.nickname!);
+        const findUser = await customUserRepo.findNickName(req.body.nickname);
         if(findUser === undefined) res.status(200).send({ success: true });
         res.status(202).send({ success: false });
 
     },
 
-    login: async (req: UserReq<UserInfo>, res: Response) => {
+    login: async (req: typeReq<strProps>, res: Response) => {
 
         const customUserRepo = getCustomRepository(UserRepo)
         const { email, password } = req.body;
@@ -54,12 +54,12 @@ const user = {
         if(findUser === undefined) return res.status(202).send({ 
             success: false, message: '존재하지 않는 이메일입니다' 
         });
-        else if(!findUser.checkPass(password!)) res.status(202).send({ 
+        else if(!findUser.checkPass(password)) res.status(202).send({ 
             success: false, message: '비밀번호가 일치하지 않습니다' 
         });
 
-        const accToken = jwtToken.mintAccessToken({id: findUser.id, email: email});
-        const refToken = jwtToken.mintRefreshToken({id: findUser.id, email: email });
+        const accToken = jwtToken.mintAccessToken(findUser.id);
+        const refToken = jwtToken.mintRefreshToken(findUser.id);
         customUserRepo.saveRefToken(findUser.id, refToken);
         
         res.cookie('accessToken', accToken, { httpOnly: true, sameSite: 'none', secure: true });
@@ -67,11 +67,11 @@ const user = {
 
     },
 
-    logout: (req: UserReq<UserInfo>, res: Response) => {
+    logout: (req: typeReq<strProps>, res: Response) => {
         
         const customUserRepo = getCustomRepository(UserRepo)
         try {
-            customUserRepo.removeRefToken(req.body.id!)
+            customUserRepo.removeRefToken(req.body.id)
             res.status(200).clearCookie('accessToken').send({ 
                 success: true, message: '로그아웃 되었습니다' 
             });
@@ -79,11 +79,11 @@ const user = {
         
     },
 
-    userInfo: async (req: UserReq<UserInfo>, res: Response) => {
-        
+    userInfo: async (req: typeReq<strProps>, res: Response) => {
+                          
         const customUserRepo = getCustomRepository(UserRepo);
         try {
-            const findUser = await customUserRepo.findId(req.body.id!);
+            const findUser = await customUserRepo.findId(req.body.id);
             if(!findUser) return res.status(202).send({ success: false, data: null });
             
             const { id, email, nickname, image, created_at, updated_at } = findUser;
@@ -105,3 +105,8 @@ const user = {
 }
 
 export default user;
+
+
+
+
+
