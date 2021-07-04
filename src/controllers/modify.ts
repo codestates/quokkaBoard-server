58204e69572e65e2d2fbb14adb29af164c100c94
SashainSPb <74516322+SashainSPb@users.noreply.github.com>
2021-07-05 00:47:06 +0900

    Test memberCRUD

--- src/controllers/modify.ts
+++ src/controllers/modify.ts
@@ -11,33 +11,38 @@ const modify = {
     nickname: (req: typeReq<strProps>, res: Response) => {
         
         const customUserRepo = getCustomRepository(UserRepo)
-        const { id, nickname } = req.body;
+        const { userId, nickname } = req.body;
+
         try {
-            customUserRepo.modifyNickName(id, nickname)
+            customUserRepo.modifyNickName(userId, nickname)
+            res.status(200).send({ success: true });
+            
         } catch (e) {
             res.status(204).send({ 
                 success: false, message: '잘못된 유저 정보 입니다' 
             });
         }
-        res.status(200).send({ success: true });
+        
 
     },
 
     password: async (req: typeReq<strProps>, res: Response) => {
 
-        const customUserRepo = getCustomRepository(UserRepo)
-        const { id, password, newpassword } = req.body;
-        const findUser = await customUserRepo.findId(id);
+        const userRepo = getRepository(User)
+        const { userId, password, newpassword } = req.body;
+        const findUser = await userRepo.findOne({where: {id: userId}})
         
-        if(findUser === undefined) return res.status(204).send({ 
+        if(!findUser) return res.status(204).send({ 
             success: false, message: '잘못된 유저 정보 입니다'
         });
-        else if(!findUser.checkPass(password)) res.status(202).send({ 
+        else if(!findUser.checkPass(password)) return res.status(202).send({ 
             success: false, message: '비밀번호가 일치하지 않습니다' 
         });
 
-        customUserRepo.modifyPassword(findUser.id, newpassword);
+        findUser.password = newpassword;
         findUser.hashPass();
+        userRepo.save(findUser)
+        res.status(200).send({success: true})
 
     },
 
@@ -49,12 +54,14 @@ const modify = {
     deleteUser: async (req: typeReq<strProps>, res: Response) => {
 
         const userRepo = getRepository(User);
+       
         try {
-            await userRepo.delete({ id: req.body.id });
+            userRepo.delete({ id: req.body.userId }); // 동기처리 전에 response가 이뤄짐.
+            res.status(200).clearCookie('accessToken').send({ success: true });
         } catch (e) {
             res.status(202).send({ success: false, message: e });// 클라이언트 쪽에서 핸들링이 되면 400으로 변경
         }
-        res.status(200).clearCookie('accessToken').send({ success: true });
+        
 
     },
 }
--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,14 +1,14 @@
-/* Module setting */
-import { Router } from 'express';
+// /* Module setting */
+// import { Router } from 'express';
 
-/* Import endpoint */
-import project from '@controllers/project';
+// /* Import endpoint */
+// import project from '@controllers/project';
 
-/* Endpoint routing */
-const projectRouter = Router();
-projectRouter.post('/create-project', project.createProject);
-projectRouter.post('/remove-project', project.removeProject);
-projectRouter.post('/modify-authority', project.modifyAuthority);
-projectRouter.post('/dashboard-info', project.dashBoardInfo);
+// /* Endpoint routing */
+// const projectRouter = Router();
+// projectRouter.post('/create-project', project.createProject);
+// projectRouter.post('/remove-project', project.removeProject);
+// projectRouter.post('/modify-authority', project.modifyAuthority);
+// projectRouter.post('/dashboard-info', project.dashBoardInfo);
 
-export default projectRouter;
+// export default projectRouter;
