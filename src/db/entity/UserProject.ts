0452a9c03a31bdd4b01450055aa7da0b92d9c9bf
skyupguf <skyupguf@gmail.com>
2021-07-07 12:03:37 +0900

    backup

---
+++ src/db/entity/UserProject.ts
@@ -0,0 +1,27 @@
+import "reflect-metadata";
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne} from "typeorm";
+import {User} from "./User"
+import {Project} from "./Project"
+
+@Entity()
+export class UserProject {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ nullable: true })
+    authority!: string;
+
+    @Column("uuid")
+    userId!: string;
+
+    @Column("uuid")
+    projectId!: string;
+   
+    @ManyToOne(() => User, user => user.user_project, {primary: true, onDelete:'CASCADE'})
+    user!: User;
+    
+    @ManyToOne(() => Project, project => project.user_project, {primary: true, onDelete:'CASCADE'})
+    project!: Project;
+
+}
