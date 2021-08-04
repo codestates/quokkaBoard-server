d12e199a6e959142355750a5a3dc9ab06416f31d
SashainSPb <74516322+SashainSPb@users.noreply.github.com>
2021-08-04 15:55:29 +0900

    test

--- src/middleware/checkauth.ts
+++ src/middleware/checkauth.ts
@@ -18,22 +18,22 @@ export const checkAuthority = async (req: Request, res: Response, next: NextFunc
     else {
         const expPayload = jwtToken.checkExpToken(token);
         const customUserRepo = getCustomRepository(UserRepo);
-        const findUser = await customUserRepo.findId(expPayload.id)
-        if(!findUser) return res.status(401).send({
-            success: false, message: '인증된 사용자가 아닙니다'
-        });
+        // const findUser = await customUserRepo.findId(expPayload.id)
+        // if(!findUser) return res.status(401).send({
+        //     success: false, message: '인증된 사용자가 아닙니다'
+        // });
 
-        const refPayload = jwtToken.checkRefToken(findUser.refresh_token!);
-        if(refPayload) {
-            const accToken = jwtToken.mintAccessToken(findUser.id);
-            res.cookie('accessToken', accToken, { 
-                httpOnly: true, sameSite: 'none', secure: true 
-            });
-            next();
-        } else {
-            res.status(202).send({ 
-                success: false, message: '다시 로그인해 주십시오' 
-            });
-        }
+        // const refPayload = jwtToken.checkRefToken(findUser.refresh_token!);
+        // if(refPayload) {
+        //     const accToken = jwtToken.mintAccessToken(findUser.id);
+        //     res.cookie('accessToken', accToken, { 
+        //         httpOnly: true, sameSite: 'none', secure: true 
+        //     });
+        //     next();
+        // } else {
+        //     res.status(202).send({ 
+        //         success: false, message: '다시 로그인해 주십시오' 
+        //     });
+        // }
     }
 }
